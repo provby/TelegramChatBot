e4239752d363c4a3aa4625611eb2280108a02e07
provby <37093086+provby@users.noreply.github.com>
2024-04-22 18:57:17 +0800

    Update User.py

--- User.py
+++ User.py
@@ -1,4 +1,11 @@
 from openai import OpenAI
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key="YOUR API KEY",
+    base_url="https://api.chatanywhere.tech/v1"
+    # base_url="https://api.chatanywhere.cn/v1"
+)
 from transitions import Machine
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Data import initUser, getUserPrompts
